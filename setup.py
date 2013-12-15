07614a5ccdcc5aae404508db6309cc7e26ef702e
Randall Degges <r@rdegges.com>
2013-12-14 17:53:03 -0800

    Adding a setup script.

---
+++ setup.py
@@ -0,0 +1,33 @@
+"""Our python packaging stuff."""
+
+
+from os.path import abspath, dirname, join, normpath
+
+from setuptools import setup
+
+
+setup(
+
+    # Basic package information:
+    name = 'stormpath-export',
+    version = '0.0.1',
+    scripts = ('stormpath-export', ),
+
+    # Packaging options:
+    zip_safe = False,
+    include_package_data = True,
+
+    # Package dependencies:
+    install_requires = ['docopt>=0.6.1', 'stormpath-sdk==1.0.0.beta'],
+
+    # Metadata for PyPI:
+    author = 'Randall Degges',
+    author_email = 'r@rdegges.com',
+    license = 'UNLICENSE',
+    url = 'https://github.com/rdegges/stormpath-export',
+    keywords = 'user authentication auth security api stormpath bcrypt utility',
+    description = 'Easily export your Stormpath user data.',
+    long_description = open(normpath(join(dirname(abspath(__file__)),
+        'README.md'))).read()
+
+)
