f14898592b82840520d988ab532a4f1cb4089d97
Randall Degges <r@rdegges.com>
2016-10-17 16:40:03 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
 
     # Basic package information:
     name = 'stormpath-export',
-    version = '0.1.0',
+    version = '0.1.1',
     scripts = ('stormpath-export', ),
 
     # Packaging options:
